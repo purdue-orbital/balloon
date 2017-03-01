934dccb6d3678f0296c1cf434bea7c18da15c463
cohen39 <cohen39@purdue.edu>
2017-03-01 18:21:36 -0500

    Add files via upload

---
+++ code/Master1_Mega.ino
@@ -0,0 +1,44 @@
+
+// Written by Nick Gammon
+// February 2011
+
+
+#include <SPI.h>
+
+void setup (void)
+{
+
+  digitalWrite(SS, HIGH);  // ensure SS stays high for now
+
+  // Put SCK, MOSI, SS pins into output mode
+  // also put SCK, MOSI into LOW state, and SS into HIGH state.
+  // Then put SPI hardware into Master mode and turn SPI on
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE0);
+  
+  
+}  // end of setup
+
+
+void loop (void)
+{
+
+  char c;
+
+  // enable Slave Select
+  digitalWrite(SS, LOW);    // SS is pin 10
+
+  // send test string
+  for (const char * p = "Hello, world!\n" ; c = *p; p++)
+  {
+    delay(10);
+    SPI.transfer (c);
+  }
+
+  // disable Slave Select
+  digitalWrite(SS, HIGH);
+
+  delay (1000);  // 1 seconds delay 
+}  // end of loop
