a2521c34cf6a4814cb1f1c44f1361a100f7a46b1
cohen39 <cohen39@purdue.edu>
2017-04-15 14:41:37 -0400

    Receiving Functions Update and Test
    ***defines were updated, and a method of testing receiving of radio packets was placed , commented out, in void loop()
    ***radRxBuff and supporting functions created for data coming in from radio, ardTxBuff and supporting functions created for data being sent to arduino (only TESTED radRxBuff & supporting functions)
    ***Replaced extractRadPackets with parseRadData and TESTED, however..... WARNING: PAYLOADS OF 6 BYTES DO NOT WORK FOR SOME REASON, NOT SURE IF THE ISSUE IS SENDING OR RECEVING, FOR NOW PAYLOAD WILL BE 7 BYTES
    ***Added printArray to quickly debug future problems by passing printArray the address of the first array element to be printed and the number of elements to be printed
    ***NOTE: some issues seemed to dissappear when holding the reset button down on the radio for the duration of code upload to arduino - a good practice

--- code/Radio_Master.c
+++ code/Radio_Master.c
@@ -1,3 +1,5 @@
+#include<SPI.h>
+#define RAD_ATTN 9
 #define START_DELIMITER 0
 #define LENGTH_1 1
 #define LENGTH_2 2
@@ -20,6 +22,7 @@
 #define DATA_6 19
 #define DATA_7 20
 
+#define RECEIVE_SIZE 22
 
 //Destination Addy (Other node's 64-bit MAC) 
 #define DEST_1_BYTE 0x00
@@ -61,55 +64,41 @@ int nextActBuffIndex = 0;
 int board;
 int tOutDelay;
 boolean synched;
-char rxBuff [100];
-char txBuff [100];
-volatile byte nextRxBuffIndex;
-volatile byte nextTxBuffIndex;
+uint8_t ardTxBuff[300];
+uint8_t radRxBuff[300];
+int nextArdTxBuffIndex = 0;
+int nextRadRxBuffIndex = 0;
 
 int lastcalled = 0;
 
 void setup() {
   Serial.begin (115200);   // debugging
-  digitalWrite(SS, HIGH);  // ensure SS stays high for now
-
-  // Put SCK, MOSI, SS pins into output mode
-  // also put SCK, MOSI into LOW state, and SS into HIGH state.
-  // Then put SPI hardware into Master mode and turn SPI on
-  SPI.begin ();
+  Serial.begin(9600);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
   SPI.setBitOrder(MSBFIRST);
   SPI.setDataMode(SPI_MODE0);
+  digitalWrite(SS,HIGH);
+  pinMode(RAD_ATTN,INPUT);
 
   pinMode(ATTENTION_IN, INPUT);
   pinMode(ATTENTION_OUT, OUTPUT);
 
   digitalWrite(ATTENTION_OUT, HIGH);
-  txBuff[1] = '1';
-  txBuff[2] = '2';
-  txBuff[3] = '3';
-
-  nextRxBuffIndex = 0;   // buffer empty
-  nextTxBuffIndex = 3;
 }
 
-void loop(){
-  /*if(board == 2)
-  {
-     if(millis()-lastcalled > 100 ){ writeByCommand('6'); lastcalled = millis();}
-       
-    //This is where user/automated requests (from this node) to send a packet should be processed. 
-   
-  }
-  else if(board == 1)
-  {
-      
-    //This is where user/automated requests (from this node) to send a packet should be processed. 
-   
-  }
-  writeByCommand('e');
-  extractPackets();*/
-
-    talk();
-
+void loop()
+{
+/*----------------------Receive-From-Radio-Test-------------------*/
+//  digitalWrite(SS,LOW);
+//  delay(2);
+//  for(int i = 0; i<100 && digitalRead(RAD_ATTN)==LOW;i++)
+//    addRadRxBuff(SPI.transfer(0x00));
+//  if(nextRadRxBuffIndex>0)
+//    parseRadData();
+//  delay(3);
+//  digitalWrite(SS,HIGH);
+//  delay(3); 
 }
 
 /*----------------------------------TALK--------------------------------*/
@@ -231,74 +220,106 @@ void extractUIPackets()
     return;
 }
 
-void extractRadioPackets()
+/*-------------------------PARSE-RADIO-DATA----------------------------*/
+void parseRadData()
 {
-    uint8_t data[19]; //Assuming AO = 0, getting frame 0x90 and frame 0x10 was transmitted with a payload consisting of only ADDRESS, ARG1,and ARG2. 
-    size_t n = 0;
-    char c;
-    int crc;
-    int i = 0;
-    bool isCorrupt;
-    bool isPacket;
-    bool done=0;
-    int nextStart = 0;
-    char address = 'n';
-    int tLastRead;
+  uint8_t data[RECEIVE_SIZE];
+  size_t n = 0;
+  char c;
+  int chk;
+  int i = 0;
+  bool isCorrupt;
+  bool isPacket;
+  bool done=0;
+  int nextStart = 0;
+  int nextStart2 = 0;
+  char address = 'n';
+  int k = 0;
+  int lastUsed = 0;
+  int lastMeaningful = 0;
+  int oldNextRadRx = nextRadRxBuffIndex;
 
-    while(address!='e')
+  //Serial.println("Entering parse... ");
+  
+  while(((k<oldNextRadRx && k<sizeof(radRxBuff))||(radRxBuff[oldNextRadRx-6]==START_BYTE && !(k<oldNextRadRx && k<sizeof(radRxBuff)) && done!=1))&& ((k-lastUsed)<=sizeof(data)))
+  {
+    if(k<oldNextRadRx && k<sizeof(radRxBuff))
     {
-        if(tLastRead - millis() > tOutDelay){ writeByCommand('e'); digitalWrite(13,LOW); synched = 0;} //Timeout.. haven't received a packet in a while
-        
-        if(digitalRead(ATTN)==0)//not sure if this is necessary, used to be used to make sure program wasn't getting ahead of serial stream
-        {
-            if(synched == 0){digitalWrite(13,HIGH); synched = 1;}
-            c = SPI.transfer(0); //Not sure if this is what we want to send, but we really just want to get bytes! Don't care about what is sent. Should look into the proper bytes to be sent when simply reading bytes
-            if(i == sizeof(data))//If the last byte of a possible packet is reached
-            {	
-				//NOTE this does not adapt to different sized data packets because sizeof(data) is defined and constant
-				if(data[sizeof(data)-1] == chkSum(data,sizeof(data)))//compare last byte of packet to calculated hash sum
-                {
-					i = 0;
-                    nextStart = 0;
-                    actOnPacket(data[15],data[16],data[17]); //Assuming AO = 0, getting frame 0x90 and frame 0x10 was transmitted with a payload consisting of only ADDRESS, ARG1,and ARG2. 
-                    address = data[15]; //Assuming AO = 0, getting frame 0x90 and frame 0x10 was transmitted with a payload consisting of only ADDRESS, ARG1,and ARG2.
-                }
-				
-                //checksum of packet does not pass
-                else
-                {
-                    //If there was a startbyte in middle of what was a possible packet
-                    if(nextStart!=0)
-                    {
-                        int j;
-                        for(j = nextStart;j < sizeof(data);j++)
-                        {
-                            data[j-nextStart] = data[j];
-                        }
-                        i = j - nextStart;
-                        nextStart = 0;
-                    }
-                }
-            }
-        
-            if(c == START_BYTE && i != 0)
-            {
-                if(i>sizeof(data))
-                    {
-                        i = 0;
-                        nextStart = 0;
-                    }
-                else if(nextStart==0)
-                        nextStart = i;   
-            }
-		
-            if(i<sizeof(data))
-                data[i]=c;
-            i++;
-            tLastRead = millis();       
-        }
+      c = radRxBuff[k];
+      lastUsed = k;
     }
-    return;
+    k++;
+    if(i == sizeof(data))//If the last byte of a possible packet is reached
+    {
+      Serial.print(1);
+      //NOTE this does not adapt to different sized data packets because sizeof(data) is defined and constant
+      chk = chkSum(&data[0], sizeof(data));
+      Serial.print("Generated chk: ");
+      Serial.print(chk,HEX);
+      Serial.print(" chk found in packet: ");
+      Serial.println(data[sizeof(data)-1],HEX);
+      if(data[sizeof(data)-1]==chk)//compare last byte of packet to calculated hash sum
+      {
+        Serial.print(2);
+        i = 0;
+        nextStart = 0;
+        addArdTxBuff(&data[14],7);//Assuming AO = 0, getting frame 0x90 and frame 0x10 was transmitted with a payload consisting of only ADDRESS, ARG1,and ARG2. 
+        printArray(&data[14],7);
+        address = data[14]; //Assuming AO = 0, getting frame 0x90 and frame 0x10 was transmitted with a payload consisting of only ADDRESS, ARG1,and ARG2.
+        done = 1;
+        lastMeaningful = lastUsed-1;//c hasn't actually been added to data[] yet, so the last byte that we know has meaning is the byte before lastUsed. c is added near the end of this loop...
+      }  
+      else//checksum of packet does not pass
+      {
+        Serial.print(3);
+        //for(int index = 0; index<sizeof(data);index++) 
+          //Serial.print((char) data[index]);
+        if(nextStart!=0)//If there was a startbyte in middle of what was a possible packet
+        {
+          //Serial.print(4);
+          int j;
+          for(j = nextStart;j < sizeof(data);j++)
+          {
+            //Serial.print(5);
+            if(data[j]==START_BYTE) nextStart2 = j-nextStart; //if there is another start even after shifting everything over....
+            data[j-nextStart] = data[j];
+          }
+          i = j - nextStart;
+          nextStart = nextStart2;
+        }
+        done = 0;
+      }
+    }
+    
+    if(c == START_BYTE && i != 0)
+    {
+      //Serial.print(6);
+      if(i>sizeof(data))
+      {
+        //Serial.print(7);
+        i = 0;
+        nextStart = 0;
+      }
+      else if(nextStart==0)
+      {
+        nextStart = i;
+        //Serial.print(8);
+      }
+    }
+
+    if(i<sizeof(data))
+    {
+      data[i]=c;
+      //Serial.print(9);
+      i++;
+    }
+  }
+  for(k = lastMeaningful+1; k<nextRadRxBuffIndex && k<sizeof(radRxBuff);k++)
+  {
+    radRxBuff[k-lastMeaningful-1] = radRxBuff[k];
+  }
+  nextRadRxBuffIndex = k-lastMeaningful-1;
+  return;
 }
 
 void writeByCommand(char address)
@@ -383,74 +404,35 @@ void writeData(uint8_t payload[],int len)
   //Serial.println("");
 }
 
-void actOnPacket(char address, char dataOne, char dataTwo)
+/*--------------------------------BUFFER----------------------------------*/
+/*----------------------------------ADDARDTXBUFF--------------------------------*/
+void addArdTxBuff(uint8_t data[],int size)
 {
-  /* wdt_reset(); */
-    switch(address)
-    {
-        case '1':
-            //printf("Sensor 1 data: %d mph, %d degrees\n",(int)dataOne,(int)dataTwo);
-            /* Serial1.print("Sensor 1 data: "); */
-            /* Serial1.print((int)dataOne); */
-            /* Serial1.print(" mph, "); */
-            /* Serial1.print((int)dataTwo); */
-            /* Serial1.print(" degrees\n"); */
-            break;
-        case '2':
-            Serial.print("Identity of remote node: board_");
-            Serial.print((int)dataOne);
-            Serial.print(" I'm board_");
-            Serial.println(board);
-            /* Serial1.print("Sensor 2 data: "); */
-            /* Serial1.print((int)dataOne); */
-            /* Serial1.print(" K\n"); */
-            break;
-        case '3':
-            //printf("Sensor 3 data: %d N\n",(int)dataOne);
-            /* Serial1.print("Sensor 3 data: "); */
-            /* Serial1.print((int)dataOne); */
-            /* Serial1.print("N\n"); */
-            break;
-        case '4':
-            //printf("Sensor 4 data: %d m\n",(int) dataOne);
-            /* Serial1.print("Sensor 4 data: "); */
-            /* Serial1.print((int)dataOne); */
-            /* Serial1.print(" m\n"); */
-            break;
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-            addActOnBuff(address-4);
-            break;
-		case 'c':
-			Serial1.print("Request to move motor %c",dataOne);
-			Serial1.print(" to ");
-			Serial1.println(((int) dataTwo)*360/255);
-			break;
-        case 'e':
-            delay(SWITCHING_DELAY);
-            flushActOnBuff();
-            break;
-    }
-}
-
-void addActOnBuff(char address)
-{
-  /* Serial1.println("\nADDED SOMETHING TO BUFFER"); */
-  if(nextActBuffIndex<sizeof(actOnBuff))
-    actOnBuff[nextActBuffIndex++] = address;
-  return;
-}
-
-void flushActOnBuff()
-{
-  for(int i=0; i<nextActBuffIndex&&i<sizeof(actOnBuff);i++)
+  for(int i = 0; i<size && nextArdTxBuffIndex<sizeof(ardTxBuff);i++)
   {
-    writeByCommand(actOnBuff[i]);
+    ardTxBuff[nextArdTxBuffIndex++] = data[i];
+    //Serial.print(data[i]);
+    //Serial.println(" was added to ardTxBuff");
   }
-  nextActBuffIndex = 0;
   return;
+}
+/*----------------------------------ADDRADRXBUFF--------------------------------*/
+void addRadRxBuff(char data)
+{
+  if(nextRadRxBuffIndex < sizeof(radRxBuff))
+  {
+    radRxBuff[nextRadRxBuffIndex++] = data;
+  }
+  else
+  {
+    nextRadRxBuffIndex = 0;
+    radRxBuff[nextRadRxBuffIndex++] = data;
+  }
+  Serial.print(data,HEX);
+  Serial.print(" ");
+  Serial.print((char) data);
+  Serial.println(" was added to radRxBuff");
+  return;  
 }
 
 uint8_t chkSum(uint8_t data[], int size) //Shouldn't use for packets larger than 260 bytes total in size, unsigned int sum may rollover. Technically should still work though...?
@@ -486,4 +468,20 @@ uint16_t crc16(uint8_t *data_p, unsigned short length)
       crc = (crc << 8) | (data >> 8 & 0xff);
  
       return (crc);
+}
+
+/*------------Print-Array-----------------*/
+void printArray(uint8_t data[],int size)
+{
+  for(int i=0;i<size;i++)
+  {
+    Serial.print((char) data[i]);
+    Serial.println(",");
+  }
+}
+uint8_t chkSum(uint8_t data[], int size) //Shouldn't use for packets larger than 260 bytes total in size, unsigned int sum may rollover. Technically should still work though...?
+{
+  unsigned int sum = 0;
+  for(int i=3;i<(size-1);i++)sum += data[i];//add all bytes except start delimeter, length bytes, and checksum byte
+  return 0xFF-(uint8_t)sum;                 //return checksum: 0xFF minus this sum ^
 }
