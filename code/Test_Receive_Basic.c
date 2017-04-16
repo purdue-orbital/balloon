b43f5b68a71eb7152c09d6f7616997c6f046b3ee
cohen39 <cohen39@purdue.edu>
2017-04-16 05:10:41 -0400

    Update Test_Receive_Basic.c

--- code/Test_Receive_Basic.c
+++ code/Test_Receive_Basic.c
@@ -1,4 +1,5 @@
 #include<SPI.h>
+#define LED_PIN 8
 #define RAD_ATTN 9
 #define START_DELIMITER 0
 #define LENGTH_1 1
@@ -64,6 +65,7 @@ void setup()
   SPI.setDataMode(SPI_MODE0);
   digitalWrite(SS,HIGH);
   pinMode(RAD_ATTN,INPUT);
+  pinMode(LED_PIN,OUTPUT);
 }
 void loop()
 {
@@ -76,6 +78,8 @@ void loop()
     parseRadData();
   delay(3);
   digitalWrite(SS,HIGH);
+  if(nextArdTxBuffIndex>0)
+    parseTxData();
   delay(3);
   
 }
@@ -226,4 +230,140 @@ uint8_t chkSum(uint8_t data[], int size) //Shouldn't use for packets larger than
   for(int i=3;i<(size-1);i++)sum += data[i];//add all bytes except start delimeter, length bytes, and checksum byte
   return 0xFF-(uint8_t)sum;                 //return checksum: 0xFF minus this sum ^
 }
+/*--------------------------------PARSE----------------------------------*/
+void parseTxData()
+{
+  uint8_t data[6];
+  size_t n = 0;
+  char c;
+  int crc;
+  int i = 0;
+  bool isCorrupt;
+  bool isPacket;
+  bool done=0;
+  int nextStart = 0;
+  int nextStart2 = 0;
+  char address = 'n';
+  int k = 0;
+  int lastUsed = 0;
+  int lastMeaningful = 0;
+  int oldNextTx = nextArdTxBuffIndex;
+  
+  while((k<oldNextTx && k<sizeof(ardTxBuff))||(ardTxBuff[oldNextTx-6]=='@' && !(k<oldNextTx && k<sizeof(ardTxBuff)) && done!=1))
+  {
+    if(k<oldNextTx && k<sizeof(ardTxBuff))
+    {
+      c = ardTxBuff[k];
+      lastUsed = k;
+    }
+    k++;
+    if(i == sizeof(data))//If the last byte of a possible packet is reached
+    {
+      //Serial.print(1);
+      //NOTE this does not adapt to different sized data packets because sizeof(data) is defined and constant
+      crc = crc16(&data[1], 3);
+      if(data[4]==(uint8_t) (crc & 0xFF)&& data[5] == (uint8_t) ((crc >> 8) & 0xFF))//compare last byte of packet to calculated hash sum
+      {
+        //Serial.print(2);
+        i = 0;
+        nextStart = 0;
+        actOnPacket(data[1],data[2],data[3]);//Assuming AO = 0, getting frame 0x90 and frame 0x10 was transmitted with a payload consisting of only ADDRESS, ARG1,and ARG2. 
+        address = data[1]; //Assuming AO = 0, getting frame 0x90 and frame 0x10 was transmitted with a payload consisting of only ADDRESS, ARG1,and ARG2.
+        done = 1;
+        lastMeaningful = lastUsed-1;//c hasn't actually been added to data[] yet, so the last byte that we know has meaning is the byte before lastUsed. c is added near the end of this loop...
+      }  
+      else//checksum of packet does not pass
+      {
+        //Serial.print(3);
+        //for(int index = 0; index<sizeof(data);index++) 
+          //Serial.print((char) data[index]);
+        if(nextStart!=0)//If there was a startbyte in middle of what was a possible packet
+        {
+          //Serial.print(4);
+          int j;
+          for(j = nextStart;j < sizeof(data);j++)
+          {
+            //Serial.print(5);
+            if(data[j]=='@') nextStart2 = j-nextStart; //if there is another start even after shifting everything over....
+            data[j-nextStart] = data[j];
+          }
+          i = j - nextStart;
+          nextStart = nextStart2;
+        }
+        done = 0;
+      }
+    }
+    
+    if(c == '@' && i != 0)
+    {
+      //Serial.print(6);
+      if(i>sizeof(data))
+      {
+        //Serial.print(7);
+        i = 0;
+        nextStart = 0;
+      }
+      else if(nextStart==0)
+      {
+        nextStart = i;
+        //Serial.print(8);
+      }
+    }
 
+    if(i<sizeof(data))
+    {
+      data[i]=c;
+      //Serial.print(9);
+      i++;
+    }
+  }
+  noInterrupts();
+  for(k = lastMeaningful+1; k<nextArdTxBuffIndex && k<sizeof(ardTxBuff);k++)
+  {
+    ardTxBuff[k-lastMeaningful-1] = ardTxBuff[k];
+  }
+  nextArdTxBuffIndex = k-lastMeaningful-1;
+  interrupts();
+  return;
+}
+
+void actOnPacket(uint8_t address, uint8_t arg1, uint8_t arg2)
+{
+  switch(address)
+  {
+    case 'c':
+      if(arg1=='o' && arg2 == 'n')
+        digitalWrite(LED_PIN,HIGH);
+        delay(1000);
+        digitalWrite(LED_PIN,LOW);
+      break;
+  }
+  return;
+}
+uint16_t crc16(uint8_t *data_p, unsigned short length)
+{
+      unsigned char i;
+      unsigned int data;
+      unsigned int crc = 0xffff;
+ 
+      if (length == 0)
+            return (~crc);
+ 
+      do
+      {
+            for (i=0, data=(unsigned int)0xff & *data_p++;
+                 i < 8; 
+                 i++, data >>= 1)
+            {
+                  if ((crc & 0x0001) ^ (data & 0x0001))
+                        crc = (crc >> 1) ^ POLY;
+                  else  crc >>= 1;
+            }
+      } while (--length);
+ 
+      crc = ~crc;
+      data = crc;
+      crc = (crc << 8) | (data >> 8 & 0xff);
+ 
+      return (crc);
+}
